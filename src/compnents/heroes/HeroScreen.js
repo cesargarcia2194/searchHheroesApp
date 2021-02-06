8de8588a3565ddbea838b1af4104af3b5b185319
Cesar Garcia <cesargarcia2194@gmail.com>
2021-02-06 00:19:47 -0400

    renderizando listado de heroes

--- src/compnents/heroes/HeroScreen.js
+++ src/compnents/heroes/HeroScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
 export const HeroScreen = () =>{
+
     return(
         <>
             <h1>Hero Screen</h1>
+            
         </>
     )
 }
--- src/compnents/marvel/MarvelScreen.js
+++ src/compnents/marvel/MarvelScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { HeroList } from '../heroes/HeroList';
 export const MarvelScreen = () =>{
     return(
         <>
             <h1>Marvel Screen</h1>
+            <hr />
+            <HeroList publisher="Marvel Comics"/>
         </>
     )
 }
